668fbabf4279aad06d4740a7bd9ea90866e56b99
fondotoce <fondotoce@gmail.com>
2018-12-30 20:12:31 +0200

    Got to services.

--- src/Controller/ArticleController.php
+++ src/Controller/ArticleController.php
@@ -3,22 +3,34 @@
 namespace App\Controller;
 
 
+use Psr\Log\LoggerInterface;
 use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\JsonResponse;
 use Symfony\Component\HttpFoundation\Response;
 
 class ArticleController extends AbstractController
 {
+
     /**
-     * @Route("/")
+     * @Route("/news/{slug}/heart", name="article_toggle_heart", methods={"POST"})
      */
-    public function homepage()
+    public function toggleArticleHeart($slug, LoggerInterface $logger)
     {
-        return new Response('OMG! My first page already! WOOO!');
+        $logger->info('Article is being hearted!');
+        return new JsonResponse(['hearts' => rand(5, 100)]);
     }
 
     /**
-     * @Route("/news/{slug}")
+     * @Route("/", name="app_homepage")
+     */
+    public function homepage()
+    {
+        return $this->render('article/homepage.html.twig');
+    }
+
+    /**
+     * @Route("/news/{slug}", name="article_show")
      */
     public function show($slug)
     {
@@ -32,6 +44,7 @@ class ArticleController extends AbstractController
         return $this->render('article/show.html.twig', [
             'title' => ucwords(str_replace('-', ' ', $slug)),
             'comments' => $comments,
+            'slug' => $slug,
         ]);
     }
 }
